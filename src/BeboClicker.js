57ace0b0feebe73ce82c6cf1fe8a1d86bcaa1bff
Banditebi <166956019+Banditebi@users.noreply.github.com>
2024-06-13 22:21:49 +0300

    idk

--- src/BeboClicker.js
+++ src/BeboClicker.js
@@ -1,108 +1,68 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./BeboClicker.css";
 import clickerImage from "./clicker-image.png";
-import boostImage from "./boost-image.png";
-import missionsImage from "./missions-image.png";
-import referralImage from "./referral-image.png";
 
 function formatNumberWithCommas(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function BeboClicker({ onUpgradeClick }) {
-  const [coins, setCoins] = useState(() => {
-    const savedCoins = JSON.parse(localStorage.getItem("coins"));
-    return savedCoins !== null ? savedCoins : 0;
-  });
-
-  const initialEnergy = 500;
-  const [energy, setEnergy] = useState(() => {
-    const savedEnergy = JSON.parse(localStorage.getItem("energy"));
-    const lastActive = JSON.parse(localStorage.getItem("lastActive"));
-    if (savedEnergy !== null && lastActive !== null) {
-      const now = Date.now();
-      const timeElapsed = Math.floor((now - lastActive) / 1000);
-      const energyToAdd = Math.min(initialEnergy, timeElapsed);
-      return Math.min(initialEnergy, savedEnergy + energyToAdd);
-    }
-    return savedEnergy !== null ? savedEnergy : initialEnergy;
-  });
-
-  const [maxEnergy, setMaxEnergy] = useState(() => {
-    const savedMaxEnergy = JSON.parse(localStorage.getItem("maxEnergy"));
-    return savedMaxEnergy !== null ? savedMaxEnergy : initialEnergy;
-  });
-
-  const [isUpgrading, setIsUpgrading] = useState(false);
+function BeboClicker() {
+  const [coins, setCoins] = useState(0);
   const [isBoostActive, setIsBoostActive] = useState(false);
-  const [upgradeLevel, setUpgradeLevel] = useState(() => {
-    const savedLevel = JSON.parse(localStorage.getItem("upgradeLevel"));
-    return savedLevel !== null ? savedLevel : 1;
-  });
-  const [upgradeCost, setUpgradeCost] = useState(() => {
-    const savedCost = JSON.parse(localStorage.getItem("upgradeCost"));
-    return savedCost !== null ? savedCost : 500;
-  });
+  const [upgradeLevel, setUpgradeLevel] = useState(1);
+  const [energy, setEnergy] = useState(500);
+  const [maxEnergy, setMaxEnergy] = useState(500);
+  const [clickText, setClickText] = useState("");
+  const [clickTextPosition, setClickTextPosition] = useState({ x: 0, y: 0 });
 
-  const [energyUpgradeLevel, setEnergyUpgradeLevel] = useState(() => {
-    const savedEnergyUpgradeLevel = JSON.parse(
-      localStorage.getItem("energyUpgradeLevel")
-    );
-    return savedEnergyUpgradeLevel !== null ? savedEnergyUpgradeLevel : 1;
-  });
-  const [energyUpgradeCost, setEnergyUpgradeCost] = useState(() => {
-    const savedEnergyUpgradeCost = JSON.parse(
-      localStorage.getItem("energyUpgradeCost")
-    );
-    return savedEnergyUpgradeCost !== null ? savedEnergyUpgradeCost : 500;
-  });
-
-  const clickPositionRef = useRef({ x: 0, y: 0 });
+  const clickCountRef = useRef(0);
 
   useEffect(() => {
-    const saveData = () => {
-      localStorage.setItem("coins", JSON.stringify(coins));
-      localStorage.setItem("energy", JSON.stringify(energy));
-      localStorage.setItem("maxEnergy", JSON.stringify(maxEnergy));
-      localStorage.setItem("lastActive", Date.now());
-      localStorage.setItem("upgradeLevel", JSON.stringify(upgradeLevel));
-      localStorage.setItem("upgradeCost", JSON.stringify(upgradeCost));
-      localStorage.setItem(
-        "energyUpgradeLevel",
-        JSON.stringify(energyUpgradeLevel)
-      );
-      localStorage.setItem(
-        "energyUpgradeCost",
-        JSON.stringify(energyUpgradeCost)
-      );
-    };
-
-    saveData();
-
-    const interval = setInterval(saveData, 5000); // Сохраняем данные каждые 5 секунд
-    return () => clearInterval(interval);
-  }, [
-    coins,
-    energy,
-    maxEnergy,
-    upgradeLevel,
-    upgradeCost,
-    energyUpgradeLevel,
-    energyUpgradeCost,
-  ]);
-
-  useEffect(() => {
-    const energyInterval = setInterval(() => {
-      if (energy < maxEnergy) {
-        setEnergy((prevEnergy) => Math.min(prevEnergy + 100, maxEnergy));
-      }
+    const interval = setInterval(() => {
+      setEnergy((prevEnergy) => {
+        const newEnergy = Math.min(prevEnergy + 1, maxEnergy);
+        return newEnergy;
+      });
     }, 1000);
 
-    return () => clearInterval(energyInterval);
-  }, [energy, maxEnergy]);
+    return () => clearInterval(interval);
+  }, [maxEnergy]);
 
-  const handleButtonClick = () => {
-    setCoins((prevCoins) => prevCoins + upgradeLevel);
+  useEffect(() => {
+    if (clickCountRef.current > 0) {
+      setCoins((prevCoins) => {
+        const increment = clickCountRef.current;
+        const newCoins = prevCoins + increment * upgradeLevel;
+        return newCoins;
+      });
+
+      setEnergy((prevEnergy) => {
+        const decrement = clickCountRef.current;
+        const newEnergy = Math.max(prevEnergy - decrement, 0);
+        return newEnergy;
+      });
+
+      clickCountRef.current = 0; // Сбрасываем количество кликов
+    }
+  }, [upgradeLevel]);
+
+  const handleButtonClick = (event) => {
+    clickCountRef.current += 1; // Увеличиваем количество кликов на каждом касании
+    setCoins((prevCoins) => prevCoins + 1); // Увеличиваем монеты на 1 при каждом клике
+
+    // Получаем координаты клика относительно изображения
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    // Устанавливаем текст и его позицию для отображения
+    setClickText("+1");
+    setClickTextPosition({ x, y });
+
+    // Скрываем текст через 0.4 секунды
+    setTimeout(() => {
+      setClickText("");
+    }, 400);
   };
 
   const handleBoostClick = () => {
@@ -123,16 +83,27 @@ function BeboClicker({ onUpgradeClick }) {
   return (
     <div className={`app-container ${isBoostActive ? "boost-active" : ""}`}>
       <div className="black-background"></div>
-      <div className="coins-display">{formatNumberWithCommas(coins)} coins</div>
-      <div className="energy-display">Energy: {energy}</div>
+      <div className="coins-display">{formatNumberWithCommas(coins)}</div>
+      <div className="energy-display">{`${energy} / ${maxEnergy}`}</div>
       <hr className="divider" />
-      <img
-        src={clickerImage}
-        className="clicker-image"
-        onClick={handleButtonClick}
-        alt="Clicker"
-        draggable="false"
-      />
+      <div className="clicker-container">
+        <img
+          src={clickerImage}
+          className="clicker-image"
+          alt="Clicker"
+          draggable="false"
+          onClick={handleButtonClick}
+        />
+        {clickText && (
+          <div
+            className="click-text"
+            style={{ left: clickTextPosition.x, top: clickTextPosition.y }}
+          >
+            {clickText}
+          </div>
+        )}
+      </div>
+
       <div className="bottom-buttons">
         <button
           className="button boost-button"
